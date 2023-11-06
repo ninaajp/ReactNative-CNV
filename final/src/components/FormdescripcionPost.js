1c43093e82dc5883c0336b35a3b01b1453489a4c
vkolek <vkolek@udesa.edu.ar>
2023-11-06 08:54:10 -0300

    descripcion del posteo

---
+++ final/src/components/FormdescripcionPost.js
@@ -0,0 +1,41 @@
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import React, { Component } from 'react'
+// import { TextInputComponent } from 'react-native'
+
+export default class FormDescripcionPost extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            descripcionPost : ''
+        }
+    }
+  render() {
+    return (
+      <View>
+        <Text>Description</Text>
+        <View>
+            <TextInput
+            placeholder='Description post'
+            onChangeText={(text)=> this.setState({
+                descripcionPost: text
+            }) }
+            value={this.state.descripcionPost}
+            style={styles.input}
+            multiline={true}
+            numberOfLines={8}
+            />
+            <TouchableOpacity
+                style={styles.btn}
+                onPress={()=> this.props.onSubmit({
+                    descripcion: this.state.descripcionPost
+                })}
+            >
+                <Text>
+                    Send post 
+                </Text>
+            </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
+}
